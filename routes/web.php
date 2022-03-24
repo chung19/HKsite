b1044906951e9c098c85f14944f965c25339e935
Thai Nguyen <vanthainguyen179@gmail.com>
2022-03-24 15:04:39 +0700

    update1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/articleList', function () {
+    return view('articleList');
+});
+Route::get('/footer', function () {
+    return view('footer');
+});
