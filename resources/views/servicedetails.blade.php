82a57ff39e32cf48c1ff7e4fc67f1897b3a98669
HoangHiep2510 <hiepviro2017@gmail.com>
2022-03-25 17:10:10 +0700

    update 2

--- resources/views/servicedetails.blade.php
+++ resources/views/servicedetails.blade.php
@@ -16,7 +16,7 @@
     <div class="inforService">
         <div class="inforService__content">
             <h1 class="inforService__title">Mobile Apps Development</h1>
-            <img class="inforService__banner" src="./frontend/images/bannerMAD.png">
+            <img class="inforService__banner" src="./frontend/images/servicedetails/bannerMAD.png">
             <p class="inforService__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Curabitur aliquet quam id dui posuere blandit.
                 Vivamus suscipit tortor eget felis porttitor volutpat. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Lorem ipsum dolor sit amet, consectetur
                 adipiscing elit. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis
@@ -26,19 +26,19 @@
             </p>
             <h2 class="inforService__featuresTitle">Key features</h2>
             <div class="inforService__features">
-                <img src="./frontend/images/check_features.png">
+                <img src="./frontend/images/servicedetails/check_features.png">
                 <div class="inforService__feature">Custom Installation Process</div>
             </div>
             <div class="inforService__features">
-                <img src="./frontend/images/check_features.png">
+                <img src="./frontend/images/servicedetails/check_features.png">
                 <div class="inforService__feature">Customizable Interface</div>
             </div>
             <div class="inforService__features">
-                <img src="./frontend/images/check_features.png">
+                <img src="./frontend/images/servicedetails/check_features.png">
                 <div class="inforService__feature">Improved All Performance</div>
             </div>
             <div class="inforService__features">
-                <img src="./frontend/images/check_features.png">
+                <img src="./frontend/images/servicedetails/check_features.png">
                 <div class="inforService__feature">High Usablility</div>
             </div>
         </div>
@@ -62,31 +62,31 @@
                 <div class="pricingPlan__cardBody">
                     <div class="pricingPlan__cardBody-used">
                         <div class="pricingPlan__cardBody-checked">
-                            <img src="./frontend/images/check.png">
+                            <img src="./frontend/images/servicedetails/check.png">
                         </div>
                         <div class="pricingPlan__cardBody-use">Concept Development</div>
                     </div>
                     <div class="pricingPlan__cardBody-used">
                         <div class="pricingPlan__cardBody-checked">
-                            <img src="./frontend/images/check.png">
+                            <img src="./frontend/images/servicedetails/check.png">
                         </div>
                         <div class="pricingPlan__cardBody-use">UI/UX Design</div>
                     </div>
                     <div class="pricingPlan__cardBody-used ">
                         <div class="pricingPlan__cardBody-checked" style="background-color: #bfbebe;">
-                            <img src="./frontend/images/check.png">
+                            <img src="./frontend/images/servicedetails/check.png">
                         </div>
                         <div class="pricingPlan__cardBody-use" style="color: #bfbebe;">Configuration Management</div>
                     </div>
                     <div class="pricingPlan__cardBody-used">
                         <div class="pricingPlan__cardBody-checked" style="background-color: #bfbebe;">
-                            <img src="./frontend/images/check.png">
+                            <img src="./frontend/images/servicedetails/check.png">
                         </div>
                         <div class="pricingPlan__cardBody-use" style="color: #bfbebe;">App Integrition</div>
                     </div>
                     <div class="pricingPlan__cardBody-used">
                         <div class="pricingPlan__cardBody-checked" style="background-color: #bfbebe;">
-                            <img src="./frontend/images/check.png">
+                            <img src="./frontend/images/servicedetails/check.png">
                         </div>
                         <div class="pricingPlan__cardBody-use" style="color: #bfbebe;">App Integrition</div>
                     </div>
@@ -108,31 +108,31 @@
                 <div class="pricingPlan__cardBody">
                     <div class="pricingPlan__cardBody-used">
                         <div class="pricingPlan__cardBody-checked">
-                            <img src="./frontend/images/check.png">
+                            <img src="./frontend/images/servicedetails/check.png">
                         </div>
                         <div class="pricingPlan__cardBody-use">Concept Development</div>
                     </div>
                     <div class="pricingPlan__cardBody-used">
                         <div class="pricingPlan__cardBody-checked">
-                            <img src="./frontend/images/check.png">
+                            <img src="./frontend/images/servicedetails/check.png">
                         </div>
                         <div class="pricingPlan__cardBody-use">UI/UX Design</div>
                     </div>
                     <div class="pricingPlan__cardBody-used">
                         <div class="pricingPlan__cardBody-checked">
-                            <img src="./frontend/images/check.png">
+                            <img src="./frontend/images/servicedetails/check.png">
                         </div>
                         <div class="pricingPlan__cardBody-use" style="color: #bfbebe;">Configuration Management</div>
                     </div>
                     <div class="pricingPlan__cardBody-used">
                         <div class="pricingPlan__cardBody-checked" style="background-color: #bfbebe;">
-                            <img src="./frontend/images/check.png">
+                            <img src="./frontend/images/servicedetails/check.png">
                         </div>
                         <div class="pricingPlan__cardBody-use" style="color: #bfbebe;">App Integrition</div>
                     </div>
                     <div class="pricingPlan__cardBody-used">
                         <div class="pricingPlan__cardBody-checked" style="background-color: #bfbebe;">
-                            <img src="./frontend/images/check.png">
+                            <img src="./frontend/images/servicedetails/check.png">
                         </div>
                         <div class="pricingPlan__cardBody-use" style="color: #bfbebe;">App Integrition</div>
                     </div>
@@ -154,31 +154,31 @@
                 <div class="pricingPlan__cardBody">
                     <div class="pricingPlan__cardBody-used">
                         <div class="pricingPlan__cardBody-checked">
-                            <img src="./frontend/images/check.png">
+                            <img src="./frontend/images/servicedetails/check.png">
                         </div>
                         <div class="pricingPlan__cardBody-use">Concept Development</div>
                     </div>
                     <div class="pricingPlan__cardBody-used">
                         <div class="pricingPlan__cardBody-checked">
-                            <img src="./frontend/images/check.png">
+                            <img src="./frontend/images/servicedetails/check.png">
                         </div>
                         <div class="pricingPlan__cardBody-use">UI/UX Design</div>
                     </div>
                     <div class="pricingPlan__cardBody-used">
                         <div class="pricingPlan__cardBody-checked">
-                            <img src="./frontend/images/check.png">
+                            <img src="./frontend/images/servicedetails/check.png">
                         </div>
                         <div class="pricingPlan__cardBody-use">Configuration Management</div>
                     </div>
                     <div class="pricingPlan__cardBody-used">
                         <div class="pricingPlan__cardBody-checked">
-                            <img src="./frontend/images/check.png">
+                            <img src="./frontend/images/servicedetails/check.png">
                         </div>
                         <div class="pricingPlan__cardBody-use">App Integrition</div>
                     </div>
                     <div class="pricingPlan__cardBody-used">
                         <div class="pricingPlan__cardBody-checked">
-                            <img src="./frontend/images/check.png">
+                            <img src="./frontend/images/servicedetails/check.png">
                         </div>
                         <div class="pricingPlan__cardBody-use">App Integrition</div>
                     </div>
@@ -195,71 +195,71 @@
             <div class="allServices__row">
                 <div class="allServices__container">
                     <div>
-                        <img src="./frontend/images/service1.webp">
+                        <img src="./frontend/images/servicedetails/service1.webp">
                     </div>
                     <div class="allServices__overlay">
                         <div class="allServices__text">
                             <h2 class="allServices__textTitle">Digital Marketing</h2>
                             <p class="allServices__textContent">Conveniently predominate sticky alignm than low risk high yield methods of emp brand superior techno</p>
                         </div>
-                        <button class="allServices__btn"><img src="./frontend/images/next.png"></button>
-                    </div>
-                </div>
-
-                <div class="allServices__container">
-                    <img src="./frontend/images/bannerMAD.png">
-                    <div class="allServices__overlay">
-                        <div class="allServices__text">
-                            <h2 class="allServices__textTitle">Digital Marketing</h2>
-                            <p class="allServices__textContent">Conveniently predominate sticky alignm than low risk high yield methods of emp brand superior techno</p>
-                        </div>
-                    <button class="allServices__btn"><img src="./frontend/images/next.png"></button>
+                        <button class="allServices__btn"><img src="./frontend/images/servicedetails/next.png"></button>
                     </div>
                 </div>
 
                 <div class="allServices__container">
-                    <img src="./frontend/images/service3.png">
+                    <img src="./frontend/images/servicedetails/bannerMAD.png">
                     <div class="allServices__overlay">
                         <div class="allServices__text">
                             <h2 class="allServices__textTitle">Digital Marketing</h2>
                             <p class="allServices__textContent">Conveniently predominate sticky alignm than low risk high yield methods of emp brand superior techno</p>
                         </div>
-                    <button class="allServices__btn"><img src="./frontend/images/next.png"></button>
+                    <button class="allServices__btn"><img src="./frontend/images/servicedetails/next.png"></button>
+                    </div>
+                </div>
+
+                <div class="allServices__container">
+                    <img src="./frontend/images/servicedetails/service3.png">
+                    <div class="allServices__overlay">
+                        <div class="allServices__text">
+                            <h2 class="allServices__textTitle">Digital Marketing</h2>
+                            <p class="allServices__textContent">Conveniently predominate sticky alignm than low risk high yield methods of emp brand superior techno</p>
+                        </div>
+                    <button class="allServices__btn"><img src="./frontend/images/servicedetails/next.png"></button>
                     </div>
                 </div>
 
             </div>
             <div class="allServices__row">
                 <div class="allServices__container">
-                    <img src="./frontend/images/service4.png">
+                    <img src="./frontend/images/servicedetails/service4.png">
                     <div class="allServices__overlay">
                         <div class="allServices__text">
                             <h2 class="allServices__textTitle">Digital Marketing</h2>
                             <p class="allServices__textContent">Conveniently predominate sticky alignm than low risk high yield methods of emp brand superior techno</p>
                         </div>
-                        <button class="allServices__btn"><img src="./frontend/images/next.png"></button>
+                        <button class="allServices__btn"><img src="./frontend/images/servicedetails/next.png"></button>
                     </div>
                 </div>
 
                 <div class="allServices__container">
-                    <img src="./frontend/images/service5.png">
+                    <img src="./frontend/images/servicedetails/service5.png">
                     <div class="allServices__overlay">
                         <div class="allServices__text">
                             <h2 class="allServices__textTitle">Digital Marketing</h2>
                             <p class="allServices__textContent">Conveniently predominate sticky alignm than low risk high yield methods of emp brand superior techno</p>
                         </div>
-                    <button class="allServices__btn"><img src="./frontend/images/next.png"></button>
+                    <button class="allServices__btn"><img src="./frontend/images/servicedetails/next.png"></button>
                     </div>
                 </div>
 
                 <div class="allServices__container">
-                    <img src="./frontend/images/service6.png">
+                    <img src="./frontend/images/servicedetails/service6.png">
                     <div class="allServices__overlay">
                         <div class="allServices__text">
                             <h2 class="allServices__textTitle">Digital Marketing</h2>
                             <p class="allServices__textContent">Conveniently predominate sticky alignm than low risk high yield methods of emp brand superior techno</p>
                         </div>
-                    <button class="allServices__btn"><img src="./frontend/images/next.png"></button>
+                    <button class="allServices__btn"><img src="./frontend/images/servicedetails/next.png"></button>
                     </div>
                 </div>
 
