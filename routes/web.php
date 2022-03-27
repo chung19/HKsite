e6064e388f9c1e1f9ac59b8187c0e8f89b0d25aa
HoangHiep2510 <hiepviro2017@gmail.com>
2022-03-28 00:49:50 +0700

    Hiep update 1 28/3

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('contact');
+    return view('home');
 });
 Route::get('/home', function () {
     return view('home');
